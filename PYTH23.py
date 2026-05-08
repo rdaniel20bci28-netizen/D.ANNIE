1da8e6d8ddf022dfb47deffd6f8ed894b26498d0
rdaniel20bci28-netizen <rdaniel20_bci28@mepcoeng.ac.in>
2026-05-08 12:11:55 +0530

    Add files via upload

---
+++ PYTH23.py
@@ -0,0 +1,5 @@
+# Update the code below this line to solve the problem
+length = 45
+width = 76
+area = length*width
+print(area)
---
+++ PYTHCL39A.py
@@ -0,0 +1,6 @@
+# Update your code below this line
+mixed_string = 'Am I DreAmIng'
+lowercase_mixed_string = mixed_string.lower()
+uppercase_mixed_string = mixed_string.upper()
+print(lowercase_mixed_string)
+print(uppercase_mixed_string)
